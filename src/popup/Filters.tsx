7f7e18b3980f01c6efbdcf205b80cc8bf31ac89a
anon <n/a>
2020-11-10 13:43:46 -0500

    added sync scale

--- src/popup/Filters.tsx
+++ src/popup/Filters.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { FilterEntry } from "../types"
 import { filterInfos } from "../defaults/filters"
 import { SliderPlus } from "../comps/SliderPlus"
@@ -15,6 +15,7 @@ type FiltersProps = {
 }
 
 export function Filters(props: FiltersProps) {
+  const [syncScale, setSyncScale] = useState(false)
    
   return <div className={`Filters ${props.className || ""}`}>
     {props.filters.map(entry => (
@@ -30,8 +31,17 @@ export function Filters(props: FiltersProps) {
           props.onChange(produce(props.filters, d => {
             const dFilter = d.find(f => f.name === entry.name)
             dFilter.value = newValue.value
+
+            if (syncScale && dFilter.name.startsWith("scale")) {
+              d.filter(entry => entry.name.startsWith("scale")).forEach(entry => {
+                entry.value = newValue.value 
+              })
+            }
           }))
-        }}/>
+        }}
+        syncChange={entry.name.startsWith("scale") ? () => setSyncScale(!syncScale) : null}
+        syncValue={syncScale}
+      />
     ))}
   </div>
 }
@@ -41,6 +51,8 @@ type FilterProps = {
   entry: FilterEntry,
   onChange: (newValue: FilterEntry) => void,
   onMove: (down: boolean) => void,
+  syncChange?: () => void
+  syncValue?: boolean
 }
 
 export function Filter(props: FilterProps) {
@@ -50,7 +62,10 @@ export function Filter(props: FilterProps) {
   return <div className="Filter">
     <Move onMove={down => props.onMove(down)}/>
     <SliderPlus
-        label={window.gsm.filter[entry.name]}
+        label={<>
+          {window.gsm.filter[entry.name]}
+          {!props.syncChange ? null : <button onClick={() => props.syncChange()} style={{padding: "0px 5px", marginLeft: "10px"}} className={`toggle ${props.syncValue ? "active" : ""}`}>:</button>}
+        </>}
         value={entry.value}
         sliderMin={filterInfo.sliderMin}
         sliderMax={filterInfo.sliderMax}
