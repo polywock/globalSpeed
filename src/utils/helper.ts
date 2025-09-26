1a5e429cd74ddd3de267f3d140a9fcdc42e04c3b
anon <noreply>
2025-09-26 09:23:24 -0400

    minor

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -95,7 +95,7 @@ export function isMobile() {
   isMobileResult = /Mobi|Android|iPhone|iTabletPad|/i.test(navigator.userAgent)
 
     // iPad not reporting user agent correctly, so assume all that support this is iPad/iPhone (mobile)
-  if (globalThis.CSS && globalThis.CSS.supports?.("(-webkit-touch-callout: none)")) {
+  if (!isMobileResult && globalThis.CSS && globalThis.CSS.supports?.("(-webkit-touch-callout: none)")) {
     isMobileResult = true 
   }
 
