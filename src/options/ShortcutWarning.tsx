73c7d33cbda47a701c158449bbb611e67628aaa9
anon <noreply>
2026-03-30 22:43:00 -0400

    m

--- src/options/ShortcutWarning.tsx
+++ src/options/ShortcutWarning.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
 import { HiOutlineInformationCircle } from "react-icons/hi"
+import { ModalBase } from "@/comps/ModalBase"
 import "./ShortcutWarning.css"
 
 export function ShortcutWarning(props: {}) {
+	const [showModal, setShowModal] = useState(false)
+
 	return (
 		<div className="ShortcutWarning">
 			<HiOutlineInformationCircle />
-			By default, shortcuts are disabled on most websites. <span className="link">Learn how to enable shortcuts on any website.</span>
+			By default, shortcuts are disabled on most websites.{" "}
+			<span className="link" onClick={() => setShowModal(true)}>
+				Learn to enable shortcuts for a website
+			</span>
+			{showModal && (
+				<ModalBase onClose={() => setShowModal(false)} passClass="ShortcutWarningModal">
+					<img src="images/shortcut.png" />
+				</ModalBase>
+			)}
 		</div>
 	)
 }
