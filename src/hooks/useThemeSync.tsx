85641de387fead1de29e1bc2315a39f750db1c4c
anon <noreply>
2026-06-16 09:05:21 -0400

    m

--- src/hooks/useThemeSync.tsx
+++ src/hooks/useThemeSync.tsx
@@ -10,18 +10,17 @@ function applyDarkTheme(isDark: boolean) {
 	}
 }
 
+const systemIsDark = window.matchMedia("(prefers-color-scheme: dark)").matches
+
 export function useThemeSync() {
 	const [view] = useStateView({ darkTheme: true, fontSize: true })
 	useEffect(() => {
-		const darkTheme = view?.darkTheme
-
 		document.documentElement.style.setProperty("--font-size-scalar", `${view?.fontSize ?? (isMobile() ? 1.3 : 1)}`)
 
-		if (darkTheme == null) {
-			const mql = window.matchMedia("(prefers-color-scheme: dark)")
-			applyDarkTheme(mql.matches)
+		if (view?.darkTheme == null ? systemIsDark : view?.darkTheme) {
+			document.documentElement.classList.add("darkTheme")
 		} else {
-			applyDarkTheme(!!darkTheme)
+			document.documentElement.classList.remove("darkTheme")
 		}
 	}, [view?.darkTheme, view?.fontSize])
 
